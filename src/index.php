b07d3daf9e5171011d3a6b0687d8a10178cb06ec
Luryxyt <36661311+lukaszkusgithub@users.noreply.github.com>
2023-12-12 19:53:50 +0100

    index update

--- src/index.php
+++ src/index.php
@@ -2,30 +2,5 @@
 
 <h1>IoT System</h1>
 
-<!-- <div>
-    <h2>Sensor Data</h2>
-    <?php
-    // Wywołaj skrypt Pythona do odczytu danych z sensorów
-    exec('python3 ../python_scripts/read_sensors.py', $output);
-
-    // Wyświetl dane
-    foreach ($output as $line) {
-        echo "<p>$line</p>";
-    }
-    ?>
-</div>
-
-<div>
-    <h2>Perform Action</h2>
-    <?php
-    // Wywołaj skrypt Pythona do wykonania akcji
-    exec('python3 ../python_scripts/perform_action.py', $output);
-
-    // Wyświetl wynik wykonanej akcji
-    foreach ($output as $line) {
-        echo "<p>$line</p>";
-    }
-    ?>
-</div> -->
 
 <?php include 'public_html/components/footer.php'; ?>
