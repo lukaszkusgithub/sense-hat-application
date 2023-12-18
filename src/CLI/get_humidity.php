472e7b54d5873a6f51bd74cc7542ee58d673006f
Luryxyt <36661311+lukaszkusgithub@users.noreply.github.com>
2023-12-18 16:49:48 +0100

    Add CLI

---
+++ src/CLI/get_humidity.php
@@ -0,0 +1,25 @@
+<?php
+
+header('Content-Type: application/json');
+
+
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    $command = "python3 ../python_scripts/read_humidity.py";
+
+    $arguments = $_GET;
+
+    if (isset($arguments)) {
+        foreach ($arguments as $key => $value) {
+
+            switch ($key) {
+                case 'u':
+                    $command = $command . " -u " . $value;
+                    break;
+            }
+        }
+    }
+    echo shell_exec($command);
+}
+
+
+?>
